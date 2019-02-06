74ff9826edc46ae62018db3101053d3aa53a459a
Daria Kozielecka <dariakozielecka@MacBook-Air-Daria.local>
2019-02-07 00:05:02 +0100

    bug fix

--- js/generator.js
+++ js/generator.js
@@ -4,7 +4,7 @@ import { boysNames, girlsNames } from "./names.js";
 const answer = document.querySelector(".answer");
 const name = document.querySelector(".name");
 export const generator = function() {
-  if (flag === "male") {
+  if (flag === "boy") {
     let boyName = boysNames[Math.floor(Math.random() * boysNames.length)];
     answer.textContent = `${answers[Math.floor(Math.random() * answers.length)]} `;
     name.textContent = `${boyName}`;
